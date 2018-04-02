845dd74be6011939206233880fc3f6f9edfb4345
imamdigmi <imam.digmi@gmail.com>
2018-04-02 15:34:26 +0700

    Add akun profile

--- akun/models.py
+++ akun/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProfilPengguna(models.Model):
+    DIVISI_CHOICES = (
+        ("AKS", "Aksi dan Advokasi"),
+        ("EKO", "Ekonomi"),
+        ("SOS", "Sosial Kemasyarakatan"),
+        ("PEN", "Pendidikan dan kebudayaan"),
+        ("POL", "Politik dan Hukum"),
+        ("PER", "Pers"),
+        ("KEA", "Keagamaan"),
+        ("KEP", "Keperempuanan"),
+        ("KES", "Kesehatan")
+    )
+    JENIS_KELAMIN_CHOICES = (
+        ("P", "Pria"),
+        ("W", "Wanita")
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    divisi = models.CharField(max_length=3, choices=DIVISI_CHOICES)
+    nama_lengkap = models.CharField(max_length=100)
+    jenis_kelamin = models.CharField(max_length=1, default="P", choices=JENIS_KELAMIN_CHOICES)
+    nomor_handphone = models.CharField(max_length=13)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
+    def __str__(self):
+        return self.nama_lengkap
