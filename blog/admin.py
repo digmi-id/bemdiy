292c4b832b094cfcc585c4fb5c65be37ed9f499f
imamdigmi <imam.digmi@gmail.com>
2018-04-03 14:29:35 +0700

    Add blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+from .models import Artikel
+
+class ArtikelAdmin(SummernoteModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['user', 'judul', 'isi', 'gambar']}),
+        ('Informasi tanggal',   {'fields': ['tanggal_terbit'], 'classes': ['collapse']}),
+    ]
+    summernote_fields = ('isi', )
+    list_display = ('judul', 'tanggal_terbit', 'was_published_recently')
+    list_filter = ['tanggal_terbit']
+    search_fields = ['judul']
+    list_per_page = 3
+
+admin.site.register(Artikel, ArtikelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from akun.models import ProfilPengguna
+
+class Artikel(models.Model):
+    user = models.ForeignKey(ProfilPengguna, on_delete=models.CASCADE)
+    judul = models.CharField(max_length=250)
+    isi = models.TextField()
+    gambar = models.ImageField()
+    tanggal_terbit = models.DateTimeField('tanggal terbit')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = (
+            'created_at',
+            'updated_at',
+        )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.tanggal_terbit <= now
+
+    was_published_recently.admin_order_field = 'Tanggal diterbitkan'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Telah diterbitkan?'
+
+    def __str__(self):
+        return self.judul
