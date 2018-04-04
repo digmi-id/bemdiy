75f96de219120d2e3fd14540372db4df25e1377f
imamdigmi <imam.digmi@gmail.com>
2018-04-04 22:13:38 +0700

    Add templates and urls skeleton

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import get_object_or_404, render
+# from django.http import HttpResponseRedirect, HttpResponse
+# from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Artikel
+
+class IndexView(generic.ListView):
+    template_name = 'blog/list.html'
+    context_object_name = 'latest_artikel_list'
+
+    def get_queryset(self):
+        return Artikel.objects.filter(
+            tanggal_terbit__lte=timezone.now()).order_by('-tanggal_terbit')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Artikel
+    template_name = 'blog/detail.html'
+
+    def get_queryset(self):
+        return Artikel.objects.filter(tanggal_terbit__lte=timezone.now())
