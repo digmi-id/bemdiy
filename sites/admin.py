c188671ad60a5a2d7e26e24c3f40c639a74c390a
imamdigmi <imam.digmi@gmail.com>
2018-04-06 05:46:25 +0700

    Add models

--- sites/admin.py
+++ sites/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProfilOrganisasi, SosialMedia
+
+class SosialMediaInline(admin.TabularInline):
+    model = SosialMedia
+    extra = 0
+
+class ProfilOrganisasiAdmin(admin.ModelAdmin):
+    inlines = [SosialMediaInline]
+    list_display = ('nama', 'email', 'nomor_handphone')
+    list_per_page = 1
+
+
+admin.site.register(ProfilOrganisasi, ProfilOrganisasiAdmin)
--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class ProfilOrganisasi(models.Model):
+    nama = models.CharField(max_length=100)
+    alamat_lengkap = models.TextField()
+    alamat_singkat = models.CharField(max_length=100)
+    nomor_handphone = models.CharField(max_length=13)
+    email = models.EmailField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = (
+            'created_at',
+            'updated_at',
+        )
+
+    def __str__(self):
+        return self.nama
+
+class SosialMedia(models.Model):
+    profil = models.ForeignKey(ProfilOrganisasi, on_delete=models.CASCADE)
+    nama = models.CharField(max_length=200)
+    url = models.URLField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = (
+            'created_at',
+            'updated_at',
+        )
+
+    def __str__(self):
+        return self.nama
