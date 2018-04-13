59f373510560272c87b5e3933f914959f0ad41f0
imamdigmi <imam.digmi@gmail.com>
2018-04-14 03:15:55 +0700

    Add devisi

--- sites/views.py
+++ sites/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 from .models import ProfilOrganisasi, SosialMedia
+from akun.models import Devisi
 
 def home(request):
     template_name = 'sites/home.html'
-    return render(request, template_name, context=None)
+    devisi = Devisi.objects.all()
+    context = {'devisis': devisi}
+    return render(request, template_name, context)
+
 
 def about(request):
     template_name = 'sites/about.html'
