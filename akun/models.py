5844657c980cf55b2faacc001f3afd28579eac78
imamdigmi <imam.digmi@gmail.com>
2018-04-14 04:07:25 +0700

    Add devisi

--- akun/models.py
+++ akun/models.py
@@ -1,25 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class ProfilPengguna(models.Model):
-    DIVISI_CHOICES = (
-        ("AKS", "Aksi dan Advokasi"),
-        ("EKO", "Ekonomi"),
-        ("SOS", "Sosial Kemasyarakatan"),
-        ("PEN", "Pendidikan dan kebudayaan"),
-        ("POL", "Politik dan Hukum"),
-        ("PER", "Pers"),
-        ("KEA", "Keagamaan"),
-        ("KEP", "Keperempuanan"),
-        ("KES", "Kesehatan")
+class Devisi(models.Model):
+    nama = models.CharField(max_length=200)
+    deskripsi = models.TextField()
+    icon = models.CharField(
+        max_length=20,
+        help_text=
+        "Untuk melihat daftar icon kunjungi https://fontawesome.com/v4.7.0/icons"
     )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = (
+            'created_at',
+            'updated_at',
+        )
+
+    def __str__(self):
+        return self.nama
+
+class ProfilPengguna(models.Model):
     JENIS_KELAMIN_CHOICES = (
         ("P", "Pria"),
         ("W", "Wanita")
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    divisi = models.CharField(max_length=3, choices=DIVISI_CHOICES)
+    devisi = models.OneToOneField(Devisi, on_delete=models.CASCADE)
     nama_lengkap = models.CharField(max_length=100)
     jenis_kelamin = models.CharField(max_length=1, default="P", choices=JENIS_KELAMIN_CHOICES)
     nomor_handphone = models.CharField(max_length=13)
