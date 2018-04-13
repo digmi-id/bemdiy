50e4409bfdc3aaa9dd62db5eb008d20f2d354b9f
imamdigmi <imam.digmi@gmail.com>
2018-04-14 04:07:18 +0700

    Add devisi

--- akun/admin.py
+++ akun/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from akun.models import ProfilPengguna
+from akun.models import ProfilPengguna, Devisi
 
 class ProfilPenggunaInline(admin.StackedInline):
     model = ProfilPengguna
@@ -12,5 +12,13 @@ class ProfilPenggunaInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfilPenggunaInline, )
 
+
+class DevisiAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'deskripsi', 'icon')
+    list_filter = ['nama']
+    search_fields = ['nama']
+    list_per_page = 25
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Devisi, DevisiAdmin)
