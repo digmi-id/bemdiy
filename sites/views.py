63a389569921fa8cca0f4d19f0b5e17882a9fedb
imamdigmi <imam.digmi@gmail.com>
2018-04-14 02:04:08 +0700

    Add dynamic content

--- sites/views.py
+++ sites/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
+from .models import ProfilOrganisasi, SosialMedia
+
 def home(request):
     template_name = 'sites/home.html'
     return render(request, template_name, context=None)
 
 def about(request):
     template_name = 'sites/about.html'
-    return render(request, template_name, context=None)
+    profile = ProfilOrganisasi.objects.latest('updated_at')
+    context = {'profile': profile}
+    return render(request, template_name, context)
 
 def contact(request):
     template_name = 'sites/contact.html'
