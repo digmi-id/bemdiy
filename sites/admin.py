f75f7b9f3e9f6d03cc188a81654cc357f343ad67
imamdigmi <imam.digmi@gmail.com>
2018-04-16 15:05:59 +0700

    Add klien admin

--- sites/admin.py
+++ sites/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProfilOrganisasi, SosialMedia, Galeri
+from .models import ProfilOrganisasi, SosialMedia, Galeri, Klien
 
 class SosialMediaInline(admin.TabularInline):
     model = SosialMedia
@@ -17,5 +17,12 @@ class GaleriAdmin(admin.ModelAdmin):
     search_fields = ['judul']
     list_per_page = 25
 
+class KlienAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'url')
+    list_filter = ['nama']
+    search_fields = ['nama']
+    list_per_page = 25
+
 admin.site.register(ProfilOrganisasi, ProfilOrganisasiAdmin)
 admin.site.register(Galeri, GaleriAdmin)
+admin.site.register(Klien, KlienAdmin)
