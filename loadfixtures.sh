09c4e8ffabaab3f32dfea44b28dda4c8371d46e0
imamdigmi <imam.digmi@gmail.com>
2018-04-16 21:02:06 +0700

    Add load fixtures script

---
+++ loadfixtures.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python manage.py loaddata akun/fixtures/Devisi.json;
+python manage.py loaddata akun/fixtures/ProfilPengguna.json;
+python manage.py loaddata blog/fixtures/Artikel.json;
+python manage.py loaddata blog/fixtures/Kategori.json;
+python manage.py loaddata blog/fixtures/Tag.json;
+python manage.py loaddata blog/fixtures/Video.json;
+python manage.py loaddata blog/fixtures/Video.json;
+python manage.py loaddata sites/fixtures/Galeri.json;
+python manage.py loaddata sites/fixtures/ProfilOrganisasi.json;
+python manage.py loaddata sites/fixtures/SosialMedia.json;
