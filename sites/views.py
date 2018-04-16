2a0124c919629f65a99e276247c53f50463827b9
imamdigmi <imam.digmi@gmail.com>
2018-04-16 15:10:25 +0700

    Add klien view

--- sites/views.py
+++ sites/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 
-from .models import ProfilOrganisasi, SosialMedia, Galeri
+from .models import ProfilOrganisasi, SosialMedia, Galeri, Klien
 from akun.models import Devisi
 from blog.models import Artikel, Video
 
@@ -12,11 +12,13 @@ def home(request):
     video = Video.objects.latest('created_at')
     articles = Artikel.objects.filter(
         tanggal_terbit__lte=timezone.now()).order_by('-tanggal_terbit')[:4]
+    clients = Klien.objects.all()
     context = {
         'devisis': devisis,
         'galleries': galleries,
         'video': video,
-        'articles': articles
+        'articles': articles,
+        'clients': clients,
     }
     return render(request, template_name, context)
 
