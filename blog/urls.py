e6c0bdf4331ad93d601b69132f09c945c6681539
imamdigmi <imam.digmi@gmail.com>
2018-04-17 15:20:40 +0700

    Add list by category

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<str:slug>/', views.DetailView.as_view(), name='detail'),
+    path('kategori/<str:slug>', views.CategoryView.as_view(), name='category'),
 ]
