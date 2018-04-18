b7c55202cb4b015ac814a66081b607baabba5169
imamdigmi <imam.digmi@gmail.com>
2018-04-18 16:49:32 +0700

    Add usage

--- loadfixtures.sh
+++ loadfixtures.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-python manage.py loaddata akun/fixtures/Devisi.json;
+python manage.py loaddata akun/fixtures/User.json;
 python manage.py loaddata akun/fixtures/ProfilPengguna.json;
-python manage.py loaddata blog/fixtures/Artikel.json;
+python manage.py loaddata akun/fixtures/Devisi.json;
 python manage.py loaddata blog/fixtures/Kategori.json;
 python manage.py loaddata blog/fixtures/Tag.json;
+python manage.py loaddata blog/fixtures/Artikel.json;
 python manage.py loaddata blog/fixtures/Video.json;
-python manage.py loaddata blog/fixtures/Video.json;
-python manage.py loaddata sites/fixtures/Galeri.json;
 python manage.py loaddata sites/fixtures/ProfilOrganisasi.json;
 python manage.py loaddata sites/fixtures/SosialMedia.json;
+python manage.py loaddata sites/fixtures/Galeri.json;
+python manage.py loaddata sites/fixtures/Klien.json;
